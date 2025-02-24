583ba5164694ba37b89a25ee5367e35c2b859290
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-02-25 01:41:47 +0200

    Updated API URLs

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_BASE_URL = "https://inventory-app-om90.onrender.com/api"; // Use the actual backend URL
+
 /**
  * Utility function to fetch data from the server.
  * @param {string} url - The URL to fetch data from.
@@ -7,7 +9,7 @@ import axios from "axios";
  */
 const fetchData = async (url, setData = () => {}) => {
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(`${API_BASE_URL}${url}`);
         setData(response.data);
         return response.data;
     } catch (error) {
@@ -23,7 +25,7 @@ const fetchData = async (url, setData = () => {}) => {
  */
 const postData = async (url, data) => {
     try {
-        const response = await axios.post(url, data);
+        const response = await axios.post(`${API_BASE_URL}${url}`, data);
         return response.data;
     } catch (error) {
         console.error(`Error posting data to ${url}:`, error);
@@ -38,7 +40,7 @@ const postData = async (url, data) => {
  */
 const updateData = async (url, data) => {
     try {
-        const response = await axios.put(url, data);
+        const response = await axios.put(`${API_BASE_URL}${url}`, data);
         return response.data;
     } catch (error) {
         console.error(`Error updating data on ${url}:`, error);
@@ -52,7 +54,7 @@ const updateData = async (url, data) => {
  */
 const deleteData = async (url) => {
     try {
-        const response = await axios.delete(url);
+        const response = await axios.delete(`${API_BASE_URL}${url}`);
         return response.data;
     } catch (error) {
         console.error(`Error deleting data from ${url}:`, error);
