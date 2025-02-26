acae2b02249642c9aad3931f52b30716c4ff0eec
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-02-26 04:22:43 +0200

    DZGFH

--- frontend/src/utils/apiUtils.js
+++ frontend/src/utils/apiUtils.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
+console.log("API_BASE_URL:", API_BASE_URL); // Add this line to log the base URL
 
 export const fetchData = async (url) => {
     try {
