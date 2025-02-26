e51bd86ebbfb5247a273042d60d81c7effe9f75b
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-02-27 01:16:01 +0200

    deploy

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -11,4 +11,13 @@ const pool = mysql.createPool({
     ssl: { rejectUnauthorized: false },
 });
 
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Connected to the database');
+        connection.release(); // Release the connection back to the pool
+    }
+});
+
 module.exports = pool;
