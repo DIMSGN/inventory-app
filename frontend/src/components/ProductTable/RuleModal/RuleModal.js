fffb0e72d08a68ffac462af0056e762d7587b81d
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-02-28 17:43:08 +0200

    add  handle colors

--- frontend/src/components/ProductTable/RuleModal/RuleModal.js
+++ frontend/src/components/ProductTable/RuleModal/RuleModal.js
@@ -3,6 +3,10 @@ import RuleForm from "../../RuleForm/RuleForm"; // Import RuleForm
 import styles from "./RuleModal.module.css";
 
 const RuleModal = ({ currentProduct, formData, handleChange, handleSubmit, setIsRuleModalOpen }) => {
+    const handleColorChange = (selectedOption) => {
+        handleChange({ target: { name: "color", value: selectedOption.value } });
+    };
+
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modalContent}>
@@ -14,6 +18,7 @@ const RuleModal = ({ currentProduct, formData, handleChange, handleSubmit, setIs
                     setFormData={null} // No need to clear form data in modal
                     setEditingRule={null} // No need to handle editing rule in modal
                     productName={currentProduct.product_name} // Pass the product name
+                    handleColorChange={handleColorChange} // Pass the handleColorChange function
                 />
                 <button type="button" onClick={() => setIsRuleModalOpen(false)}>Cancel</button>
             </div>
